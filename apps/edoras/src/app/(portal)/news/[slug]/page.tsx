6a285f35181ee94ac9671c0c6934640c0b2abc17
Pedro Duarte <pedrohlduarte@gmail.com>
2023-12-11 23:58:53 -0300

    feat: initial news show

---
+++ apps/edoras/src/app/(portal)/news/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+export default function NewsPage() {
+  return (
+    <div>
+      <article>
+        <h2 className="text-2xl font-bold mb-4">Título do Artigo</h2>
+        <Image
+          src="/images/edoras.jpg"
+          alt="Edoras"
+          width={1200}
+          height={630}
+        />
+        <p className="mb-4">
+          Data de publicação:{' '}
+          <time dateTime="2023-01-01">1 de Janeiro de 2023</time>
+        </p>
+        <div className="text-gray-700 text-lg leading-relaxed">
+          <p>Primeiro parágrafo do artigo...</p>
+          <p>Segundo parágrafo...</p>
+        </div>
+      </article>
+    </div>
+  )
+}
