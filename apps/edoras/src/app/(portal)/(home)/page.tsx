a73731c7452e7a4924db5950381cae12341d142c
Pedro Duarte <pedrohlduarte@gmail.com>
2024-04-08 23:09:32 -0300

    feat: tests and fix home edoras

--- apps/edoras/src/app/(portal)/(home)/page.tsx
+++ apps/edoras/src/app/(portal)/(home)/page.tsx
@@ -2,25 +2,25 @@ import { Metadata } from 'next'
 
 import GithubProfile from '@/components/GithubProfile'
 
-import { Button } from '@rohan/ui'
+// import { Button } from '@rohan/ui'
 
-import { api } from '@/data/api'
-import { HomeProps } from '@/data/types/home'
+// import { api } from '@/data/api'
+// import { HomeProps } from '@/data/types/home'
 
 export const metadata: Metadata = {
   title: 'Home'
 }
 
-async function getHomeData(): Promise<HomeProps> {
-  const response = await api('/drivers')
-  const message = await response.json()
-  return message
-}
+// async function getHomeData(): Promise<HomeProps> {
+//   const response = await api('/drivers')
+//   const message = await response.json()
+//   return message
+// }
 export default async function Home() {
-  const { message } = await getHomeData()
+  // const { message } = await getHomeData()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>{message}</Button>
+      {/* <Button>{message}</Button> */}
       <GithubProfile />
     </main>
   )
