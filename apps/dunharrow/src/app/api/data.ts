55647c98574276504b5b9306a96b42d7b247ba24
Pedro Duarte <pedrohlduarte@gmail.com>
2024-05-12 20:06:54 -0300

    feat: remove backend

--- apps/dunharrow/src/app/api/data.ts
+++
@@ -1,5 +0,0 @@
-export const products = [
-  {
-    slug: 'teste'
-  }
-]
--- apps/dunharrow/src/app/api/drivers/route.ts
+++
@@ -1,5 +0,0 @@
-const delay = () => new Promise(resolve => setTimeout(resolve, 3000))
-export async function GET() {
-  await delay()
-  return Response.json({ message: 'Teste' })
-}
